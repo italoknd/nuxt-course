887b46ddbf8666ded196a5f6bcffc15c11ec77a3
Italo Pedroza <74671728+italoknd@users.noreply.github.com>
2023-01-19 16:05:28 -0400

    paginas e rotas

---
+++ pages/index.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <div class="bg-gray-100">
+      <div class="container mx-auto h-10 flex items-center mb-10 space-x-4">
+        <nuxt-link to="/a-empresa">A empresa</nuxt-link>
+        <nuxt-link to="/servicos">Serviços</nuxt-link>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+
+export default Vue.extend({
+  name: 'IndexPage',
+})
+</script>
---
+++ pages/servicos.vue
@@ -0,0 +1,28 @@
+<template>
+  <div>
+    <div class="bg-gray-100">
+      <div class="container mx-auto h-10 flex items-center mb-10">
+        
+      </div>
+    </div>
+    <div class="container mx-auto">
+      <h1 class="text-lg font-bold">Serviços oferecidos</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolore quo
+        numquam quisquam vero maxime eum incidunt? Porro facilis veritatis,
+        debitis sint maiores molestiae tempora adipisci omnis inventore hic
+        consequuntur dicta.
+      </p>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
